e3051a8301a4251dba1b9c38342516a934e2149f
Luana Sileoni <lusileoni6@gmail.com>
2022-12-11 17:41:18 -0300

    Subida Proyecto final

---
+++ shopping/src/modal.js
@@ -0,0 +1,63 @@
+import { eliminarProductoCarrito, eliminarTodoElCarrito } from "./accionesCarrito.js";
+
+const modalContenedor = document.querySelector('.modal-contenedor');
+const abrirCarrito = document.getElementById('cesta-carrito');
+const cerrarCarrito = document.getElementById('btn-cerrar-carrito');
+const modalCarrito = document.querySelector('.modal-carrito')
+
+abrirCarrito.addEventListener('click', () => {
+    modalContenedor.classList.toggle('modal-active')
+});
+
+cerrarCarrito.addEventListener('click', () => {
+    modalContenedor.classList.toggle('modal-active')
+});
+
+modalContenedor.addEventListener('click', () => {
+    cerrarCarrito.click()
+});
+
+modalCarrito.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (e.target.classList.contains('boton-eliminar')) {
+        Swal.fire({
+            title: '¿Esta seguro?',
+            text: 'Va a eliminar el producto',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                eliminarProductoCarrito(e.target.value);
+                Swal.fire(
+                    'Eliminado',
+                    'El producto ha sido eliminado',
+                    'success'
+                )
+            }
+        })
+    }else if (e.target.classList.contains('boton-eliminar-todo')) {
+        Swal.fire({
+            title: '¿Esta seguro?',
+            text: 'Va a eliminar el producto',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                eliminarTodoElCarrito();
+                Swal.fire(
+                    'Eliminado',
+                    'El producto ha sido eliminado',
+                    'success'
+                )
+            }
+        })
+    }
+});
